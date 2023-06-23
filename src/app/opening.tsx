278648fff2f665df66484aa1b2c1b312ac705257
yoshiko wright <yooooka@gmail.com>
2023-06-23 07:51:41 -0700

    animation ver2

--- src/app/opening.tsx
+++ src/app/opening.tsx
@@ -4,8 +4,6 @@ import { useEffect, useState } from "react";
 import { MdOutlineSouth } from "react-icons/md";
 import bg from "./assets/xd-pattern.png";
 import title00 from "./assets/title00.svg";
-import xd from "./assets/xd.svg";
-import Xdbg from "./xd";
 
 const titleVariants: Variants = {
   offscreen: {
@@ -27,7 +25,7 @@ export default function Opening() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(false);
-    }, 4500); // 1 second duration * 6 repeats = 6 seconds
+    }, 5200); // 1 second duration * 6 repeats = 6 seconds
 
     return () => clearTimeout(timer);
   }, []);
@@ -35,33 +33,35 @@ export default function Opening() {
   return (
     <AnimatePresence>
       {isVisible && (
-        <motion.div
-          layout
-          initial={{ opacity: 1, y: 0 }}
-          exit={{
-            opacity: 0,
-            y: "100vh",
-          }}
-          transition={{ duration: 0.6, type: "spring" }}
-          className="relative bg-gradient-to-r from-kronos-500 via-kronos-400 to-kronos-600 snap-start snap-always background-animate w-full h-full"
-        >
-          <div>
+        <div className="bg-kronos-light">
+          <motion.div
+            layout
+            initial={{ opacity: 1, y: 0 }}
+            exit={{
+              opacity: 0,
+              y: "100vh",
+              transition: {
+                duration: 0.8,
+                type: "spring",
+              },
+            }}
+            className="relative bg-gradient-to-r from-kronos-500 via-kronos-400 to-kronos-600 snap-start snap-always background-animate w-full h-full animation-delay-[2s]"
+          >
             <motion.div
               layout
-              initial={{ opacity: 1, scale: 1 }}
-              animate={{ opacity: 0.4, scale: [null, 2, 1] }}
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 1 }}
               transition={{
-                delay: 4,
-                duration: 1.2,
+                delay: 4.4,
+                duration: 0.5,
                 type: "spring",
-                bounce: 0.5,
               }}
               style={{
                 backgroundImage: `url(${bg.src})`,
                 backgroundPosition: "center",
               }}
             >
-              <section className="prose prose-stone flex flex-col h-screen justify-center items-center text-center w-full max-w-none p-12 prose-h1:text-2xl prose-h1:font-bold lg:prose-h1:text-4xl isolate">
+              <section className="prose prose-stone flex flex-col h-screen justify-center items-center text-center w-full max-w-none p-12 prose-h1:text-2xl prose-h1:font-bold lg:prose-h1:text-4xl">
                 <motion.div
                   className="not-prose mb-2"
                   initial="offscreen"
@@ -71,30 +71,32 @@ export default function Opening() {
                 >
                   <Image alt="wadai" src={title00} className="mx-auto" />
                 </motion.div>
-                <motion.h1
-                  className="px-6"
+                <h1 className="px-6 lg:typewriter">
+                  DX導入って、 何から始めればいいの？
+                </h1>
+                <motion.div
                   initial="offscreen"
                   whileInView={{
                     y: 0,
                     opacity: 1,
                     transition: {
                       duration: 0.4,
-                      delay: 1,
+                      delay: 1.6,
                     },
                   }}
                   viewport={{ once: true, amount: 0 }}
                   variants={titleVariants}
+                  className=" absolute bottom-12 left-[48%]"
                 >
-                  DX導入って、 何から始めればいいの？
-                </motion.h1>
-                <a href="#split-top" className="group no-underline">
-                  <div className="text-stone-900">Scroll</div>
-                  <MdOutlineSouth className="font-light animate-bounce text-3xl text-white group-hover:text-primary animation-delay-[2.5s] mx-auto" />
-                </a>
+                  <a href="#split-top" className="group no-underline">
+                    <div className="text-kronos-50 -rotate-90">Scroll</div>
+                    <MdOutlineSouth className="mt-4 font-thing text-3xl text-kronos-50 group-hover:text-kronos-dark animation-delay-[2.5s] mx-auto animate-bounce" />
+                  </a>
+                </motion.div>
               </section>
             </motion.div>
-          </div>
-        </motion.div>
+          </motion.div>
+        </div>
       )}
     </AnimatePresence>
   );
