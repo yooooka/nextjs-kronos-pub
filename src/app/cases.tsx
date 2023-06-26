d2bf67fc91883ec459269d592c12ccf6d8724595
yoshiko wright <yooooka@gmail.com>
2023-06-26 16:33:46 -0700

    case study component

---
+++ src/app/cases.tsx
@@ -0,0 +1,20 @@
+export default function Cases() {
+  return (
+    <div className="mx-auto prose prose-stone p-12 sm:py-16 max-w-screen-md prose-h2:text-center prose-h2:text-2xl isolate">
+      <h2 className="mb-2">導入事例</h2>
+      <span className="text-center text-kronos font-semibold block text-lg pb-4 font-outfit">
+        CASE STUDY
+      </span>
+      <h3>相談内容</h3>
+      <p>相談内容テキスト相談内容テキスト相談内容テキスト相談内容テキスト</p>
+      <h3>導入サービス</h3>
+      <p>セキュアSAMBA</p>
+      <h3>ご提案内容</h3>
+      <p>
+        提案内容テキスト提案内容テキスト提案内容テキスト提案内容テキスト提案内容テキスト
+      </p>
+      <h3>導入効果</h3>
+      <p>導入効果テキスト導入効果テキスト導入効果テキスト導入効果テキスト</p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ import Steps from "./steps";
 import Services from "./services";
 import ContactHtml from "./contact-html";
 import Menu from "./menu";
+import Cases from "./cases";
 import { MdArrowForward } from "react-icons/md";
 import { motion, Variants, MotionConfig } from "framer-motion";
 
@@ -194,26 +195,7 @@ export default function Home() {
               id="cases"
               className="bg-gradient-to-b from-kronos-50 to-kronos-50/0 text-stone-900 relative"
             >
-              <div className="mx-auto prose prose-stone p-12 sm:py-16 max-w-screen-md prose-h2:text-center prose-h2:text-2xl isolate">
-                <h2 className="mb-2">導入事例</h2>
-                <span className="text-center text-kronos font-semibold block text-lg pb-4 font-outfit">
-                  CASE STUDY
-                </span>
-                <h3>相談内容</h3>
-                <p>
-                  相談内容テキスト相談内容テキスト相談内容テキスト相談内容テキスト
-                </p>
-                <h3>導入サービス</h3>
-                <p>セキュアSAMBA</p>
-                <h3>ご提案内容</h3>
-                <p>
-                  提案内容テキスト提案内容テキスト提案内容テキスト提案内容テキスト提案内容テキスト
-                </p>
-                <h3>導入効果</h3>
-                <p>
-                  導入効果テキスト導入効果テキスト導入効果テキスト導入効果テキスト
-                </p>
-              </div>
+              <Cases />
             </section>
             <section
               id="faq"
